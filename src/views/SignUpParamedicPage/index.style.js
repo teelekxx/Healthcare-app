ee4028cc6abc62681c053eaff81722b95283d46d
nnevermine <79250310+nnevermine@users.noreply.github.com>
2023-02-17 14:56:01 +0700

    SignUpParamedicPage

---
+++ src/views/SignUpParamedicPage/index.style.js
@@ -0,0 +1,14 @@
+import styled from "styled-components/native";
+import { Colors } from "../../constants";
+export const CircleButton = styled.TouchableOpacity`
+  margin-top:60px;
+  margin-left:25px;
+  padding: 10px;
+  width: 50px;
+  height: 50px;
+  elevation:10;
+  background-color: ${Colors.white};
+  border-radius: 100px;
+  justify-content:center;
+  align-items:center;
+`;
