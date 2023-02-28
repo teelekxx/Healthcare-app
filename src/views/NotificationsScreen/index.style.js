b406942d741240ca75f23c6b536daeffd76e9f7a
Kittipon <kittipon@primo.mobi>
2023-02-28 17:10:24 +0700

    [ADD] add Notification page

--- src/views/NotificationsScreen/index.style.js
+++ src/views/NotificationsScreen/index.style.js
@@ -13,4 +13,44 @@ export const NotificationsTitle = styled.Text`
   margin-left: ${width * 0.0641}px;
 `;
 
-export const NotificationsContainer = styled.View``;
+export const NotificationsContainer = styled.View`
+  flex: 1;
+`;
+
+export const TitleContainer = styled.View`
+  flex-direction: row;
+`;
+
+export const NotificationsScrollable = styled.ScrollView`
+  height: ${height * 0.8994}px;
+  margin-left: ${width * 0.0641}px;
+  margin-right: ${width * 0.0641}px;
+`;
+
+export const NotificationBlock = styled.TouchableOpacity`
+  /* height: ${height * 0.699142}px; */
+  border-bottom-width: 1px;
+  border-color: ${Colors.blue};
+  border-style: solid;
+  margin-top: ${height * 0.04324}px;
+`;
+
+export const NotificationsName = styled.Text`
+  color: ${Colors.blue};
+  font-family: Monaco;
+  font-weight: bold;
+  font-size: 20px;
+`;
+
+export const NotificationsMassage = styled.Text`
+  color: ${Colors.grey};
+  font-family: Monaco;
+  font-size: 15px;
+`;
+
+export const NotificationsDate = styled.Text`
+  color: ${Colors.grey};
+  font-family: Monaco;
+  font-size: 15px;
+  margin-left: auto;
+`;
