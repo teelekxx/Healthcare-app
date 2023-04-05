8f3172a0eb9d056bf9951aa3999b15201ae54372
Kittipon <kittipon@primo.mobi>
2023-04-05 16:27:42 +0700

    [ADD] Forget password page

--- src/views/SignInPage/index.js
+++ src/views/SignInPage/index.js
@@ -1,14 +1,30 @@
-import {useState} from "react";
+import { useState } from "react";
 import { SafeAreaView, Text, Alert } from "react-native";
-import { ThemeButton, ThemeButtonText, Or, ForgotPassword, CircleButton } from "./index.style";
-import { Input, InputGroup, SubTitle, ItalicText2, BlueContainer, Form, BlueButton, BlueButtonText} from "../../components/components/index.style";
+import {
+  ThemeButton,
+  ThemeButtonText,
+  Or,
+  ForgotPassword,
+  CircleButton,
+} from "./index.style";
+import {
+  Input,
+  InputGroup,
+  SubTitle,
+  ItalicText2,
+  BlueContainer,
+  Form,
+  BlueButton,
+  BlueButtonText,
+} from "../../components/components/index.style";
 import { Icon } from "react-native-elements";
 import { Colors } from "../../constants";
 import BackButton from "../../components/BackButton";
 import Auth from "../../api/auth";
+
 function SignInPage({ navigation }) {
-  const [email, setEmail] =useState("");
-  const [password,setPassword] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   function createAlert(message) {
     Alert.alert("Try Again", message, [
       {
@@ -18,65 +34,77 @@ function SignInPage({ navigation }) {
     ]);
   }
   const handleSubmit = async (event) => {
-    try{
-      const res = await Auth.login(email, password)
-      console.log(res)
-      navigation.navigate("HomePage")
-    }catch(err){
-      if(err.code =="auth/user-not-found"){
-        createAlert("You don't have an account yet.")
+    try {
+      const res = await Auth.login(email, password);
+      console.log(res);
+      navigation.navigate("HomePage");
+    } catch (err) {
+      if (err.code == "auth/user-not-found") {
+        createAlert("You don't have an account yet.");
+      } else if (err.code == "auth/wrong-password") {
+        createAlert("Wrong password.");
       }
-      else if(err.code =="auth/wrong-password"){
-        createAlert("Wrong password.")
-      }
-      console.log(err.code)
+      console.log(err.code);
     }
-  }
+  };
   return (
     <BlueContainer>
       <CircleButton onPress={() => navigation.goBack()}>
-            <Icon
-              name="arrow-back-outline"
-              type="ionicon"
-              color={Colors.blue}
-              size={20}
-            />
-        </CircleButton>
-      <Form>
-      <SubTitle>Welcome back!</SubTitle>
-      <ItalicText2>Hello there, sign in to continue</ItalicText2>
-      <ThemeButton>
-      <Icon
-              name="logo-google"
-              type="ionicon"
-              color={Colors.grey}
-              size={20}
-            />
-        <ThemeButtonText>Sign in with Google</ThemeButtonText>
-      </ThemeButton>
-      <Or>or</Or>
-        <InputGroup>
         <Icon
-              name="mail-outline"
-              type="ionicon"
-              color={Colors.grey}
-              size={30}
-            />
-          <Input type="text" placeholder="Email address" placeholderTextColor={Colors.grey}  onChangeText={setEmail}
-          value={email}/>
-        </InputGroup>
-        <InputGroup>
-        <Icon
-          name='lock-closed-outline'
-          type='ionicon'
-          color={Colors.grey}
-          size={30}
+          name="arrow-back-outline"
+          type="ionicon"
+          color={Colors.blue}
+          size={20}
         />
-          <Input type="text"  secureTextEntry={true} placeholder="Password" placeholderTextColor={Colors.grey}  onChangeText={setPassword}
-          value={password}/>
+      </CircleButton>
+      <Form>
+        <SubTitle>Welcome back!</SubTitle>
+        <ItalicText2>Hello there, sign in to continue</ItalicText2>
+        <ThemeButton>
+          <Icon
+            name="logo-google"
+            type="ionicon"
+            color={Colors.grey}
+            size={20}
+          />
+          <ThemeButtonText>Sign in with Google</ThemeButtonText>
+        </ThemeButton>
+        <Or>or</Or>
+        <InputGroup>
+          <Icon
+            name="mail-outline"
+            type="ionicon"
+            color={Colors.grey}
+            size={30}
+          />
+          <Input
+            type="text"
+            placeholder="Email address"
+            placeholderTextColor={Colors.grey}
+            onChangeText={setEmail}
+            value={email}
+          />
         </InputGroup>
-        <ForgotPassword>Forgot password?</ForgotPassword>
-        <BlueButton  onPress={handleSubmit}>
+        <InputGroup>
+          <Icon
+            name="lock-closed-outline"
+            type="ionicon"
+            color={Colors.grey}
+            size={30}
+          />
+          <Input
+            type="text"
+            secureTextEntry={true}
+            placeholder="Password"
+            placeholderTextColor={Colors.grey}
+            onChangeText={setPassword}
+            value={password}
+          />
+        </InputGroup>
+        <ForgotPassword onPress={() => navigation.navigate("ForgetPassword")}>
+          Forgot password?
+        </ForgotPassword>
+        <BlueButton>
           <BlueButtonText>Sign in</BlueButtonText>
         </BlueButton>
       </Form>
@@ -84,4 +112,3 @@ function SignInPage({ navigation }) {
   );
 }
 export default SignInPage;
-
